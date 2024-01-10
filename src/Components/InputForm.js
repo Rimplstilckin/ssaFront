f93cd28f679c9a19f5f02ec69b01706fe06c26ec
Rimplstilckin <milos.protic@gmail.com>
2024-01-10 23:26:05 +0100

    Done!

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -3,16 +3,87 @@ import { TextField } from "@mui/material";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
+import axios from 'axios';
+import {useState} from "react";
+
 // css
 import '../css/inputForm.css';
 
+const address = 'https://85r6g5q0uf.execute-api.eu-central-1.amazonaws.com/default/createWorker'
+
+const addWorker = (worker) => {
+    axios.post(address, {
+        "firstName": worker.firstName,
+        "lastName": worker.lastName,
+        "born": worker.born,
+        "phone": worker.phone,
+        "position": worker.position,
+        "education": worker.education,
+        "experience": worker.experience,
+        "note": worker.note
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
 const InputFields = (props) => {
 
-    // const [value, setValue] = useState(props.value);
+    const [firstName, setFirstName] = useState("");
+    const changeFirstNameHandler = e => {
+        setFirstName(e.target.value);
+    }
 
-    // const clickHanler = () => {
-    //     setValue('')
-    // }
+    const [lastName, setLastName] = useState("");
+    const changeLastNameHandler = e => {
+        setLastName(e.target.value);
+    }
+
+
+    const [born, setBornDate] = useState("");
+    const changeBornHandler = e => {
+        setBornDate(e.target.value);
+    }
+
+    const [phone, setPhone] = useState("");
+    const changePhoneHandler = e => {
+        setPhone(e.target.value);
+    }
+
+    const [experience, setExperience] = useState("");
+    const changeExperienceHandler = e => {
+        setExperience(e.target.value);
+    }
+    
+    const [position, setPosition] = useState("");
+    const changePositionHandler = e => {
+        setPosition(e.target.value);
+    }
+    
+    const [education, setEducation] = useState("");
+    const changeEducationHandler = e => {
+        setEducation(e.target.value);
+    }
+    
+    const [note, setNote] = useState("");
+    const changeNoteHandler = e => {
+        setNote(e.target.value);
+    }
+
+    const worker = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "born": born,
+        "phone": phone,
+        "position": position,
+        "education": education,
+        "experience": experience,
+        "note": note
+    }
+    
 
     return (
         <div className="container">
@@ -20,22 +91,14 @@ const InputFields = (props) => {
             <form method="Post" action="backendHanler.php">
                 <div className='form'>
 
-                    <div className="inputField idBroj">
-                        <TextField
-                            required
-                            label='ID zaposlenog'
-                            variant='standard'                            
-                            type="number"
-                            name='idBroj'
-                        />
-                    </div>
-
                     <div className="inputField firstName">
                         <TextField
                             required
                             label='Ime'
                             variant='standard'
                             name='firstName'
+                            value={firstName}
+                            onChange={changeFirstNameHandler}
                         />
                     </div>
 
@@ -45,6 +108,8 @@ const InputFields = (props) => {
                             label='Prezime'
                             variant='standard'
                             name='lastName'
+                            value={lastName}
+                            onChange={changeLastNameHandler}
                         />
                     </div>
                     
@@ -57,6 +122,8 @@ const InputFields = (props) => {
                             variant='standard'
                             type="date"
                             name='born'
+                            value={born}
+                            onChange={changeBornHandler}
                         />
                     </div>
 
@@ -64,7 +131,9 @@ const InputFields = (props) => {
                         <TextField
                             label='Broj telefona'
                             variant='standard'
-                            name='phone'
+                            name='phone'                            
+                            value={phone}
+                            onChange={changePhoneHandler}
                         />
                     </div>
 
@@ -74,6 +143,8 @@ const InputFields = (props) => {
                             label='Godine iskustva'
                             variant='standard'
                             name='experience'
+                            value={experience}
+                            onChange={changeExperienceHandler}
                         />
                     </div>
 
@@ -83,6 +154,8 @@ const InputFields = (props) => {
                             label='Radno mesto'
                             variant='standard'
                             name='position'
+                            value={position}
+                            onChange={changePositionHandler}
                         />
                     </div>
 
@@ -92,6 +165,8 @@ const InputFields = (props) => {
                             label='Stručna sprema'
                             variant='standard'
                             name='education'
+                            value={education}
+                            onChange={changeEducationHandler}
                         />
                     </div>
 
@@ -102,13 +177,15 @@ const InputFields = (props) => {
                             multiline
                             maxRows={4}
                             name='note'
+                            value={note}
+                            onChange={changeNoteHandler}
                         />
                     </div>
 
                     <div className="buttons">
                         <Stack spacing={2} direction="row">
                             <Button variant="outlined">Cancel</Button>
-                            <Button type='submit' variant="contained">Submit</Button>
+                            <Button type='submit' variant="contained" onClick={() => addWorker(worker)}>Submit</Button>
                         </Stack>
                     </div>
 
